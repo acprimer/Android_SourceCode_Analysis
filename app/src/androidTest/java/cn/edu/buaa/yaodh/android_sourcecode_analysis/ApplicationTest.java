3a6f0fe2723c150d70d1caa7de430e36b0b4b154
acprimer <729101421@qq.cm>
2014-12-07 21:01:39 +0800

    Android_SourceCode_Analysis

---
+++ app/src/androidTest/java/cn/edu/buaa/yaodh/android_sourcecode_analysis/ApplicationTest.java
@@ -0,0 +1,13 @@
+package cn.edu.buaa.yaodh.android_sourcecode_analysis;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
